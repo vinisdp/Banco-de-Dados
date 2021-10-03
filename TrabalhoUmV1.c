94d738662e7a1f4378238b6a1a84dc5422a6e57e
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2021-10-02 22:37:50 -0400

    Add files via upload

--- TrabalhoUmV1.c
+++ TrabalhoUmV1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void leitura_parametro(char *nomeInstancia, int *orcamento, int *saida, char *dimencao, FILE *entrada)
+/*Função para fazer a leitura das informações da arena, ela recebe por
+parametro as variaveis e o arquivo de entrada, apos isso é criada uma
+variavel para armazenar as informações que não são uteis para o programa
+e a leitura é feita utilizando o fscanf*/
+void leitura_parametro(char *nomeInstancia, int *orcamento, int *saida, char *dimensao, FILE *entrada)
 {
     char lixo[30];
     fscanf(entrada, "%s", lixo);
@@ -14,13 +18,17 @@ void leitura_parametro(char *nomeInstancia, int *orcamento, int *saida, char *di
     fscanf(entrada, "%d", saida);
     printf("%d\n", *saida);
     fscanf(entrada, "%s", lixo);
-    fscanf(entrada, "%s", dimencao);
+    fscanf(entrada, "%s", dimensao);
 }
-void aloca_arena(char **arena, int coluna)
+/*Função para alocar os vetores que serão utilizados para
+coluna da matriz da arena nela é recebido quantidade de linhas,
+colunas e a propria matriz e aloca os vetores com o tamanho das
+colunas em cada linha do vertor que armazena os endereços*/
+void aloca_arena(char **arena, int coluna, int linha)
 {
     int i;
     /*Pedi memoria ao sistema*/
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < linha; i++)
     {
         arena[i] = (char *)malloc(coluna * sizeof(char));
         if (arena[i] == NULL)
@@ -30,32 +38,41 @@ void aloca_arena(char **arena, int coluna)
         }
     }
 }
-void libera_arena(char **Arena)
+/*Função para liberar a memoria alocada para matriz
+arena utilizando a função arena*/
+void libera_arena(char **Arena,int linha)
 {
     int y;
-    for (y = 0; y < 6; y++)
+    for (y = 0; y < linha; y++)
     {
         free(Arena[y]);
     }
     free(Arena);
 }
-void leitura_arena(char **Arena, FILE *entrada)
+/*Função para a leitura das linhas da arena
+de dentro do arquivo usando o fscanf*/
+void leitura_arena(char **Arena, FILE *entrada, int linha)
 {
     int y;
 
-    for (y = 0; y < 6; y++)
+    for (y = 0; y < linha; y++)
     {
         fscanf(entrada, " %[^\n]", Arena[y]);
     }
 }
-void mostrar_arena(char **arena)
+/*Função para imprimir a arena*/
+void mostrar_arena(char **arena, int linha)
 {
     int i;
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < linha; i++)
     {
         printf("%s\n", arena[i]);
     }
 }
+/*Nessa função é utilizado o strtok para separar a quantidade
+de linhas e colunas essa função permite que escolhemos caracteres
+chaves para que haja a separação das duas partes da string
+alem disso também foi utilizado o atoi*/
 void separa_dimencao(int *linha, int *coluna, char *dimencao)
 {
     char *pt;
@@ -78,23 +95,23 @@ void separa_dimencao(int *linha, int *coluna, char *dimencao)
 }
 int main(int argc, char *argv[])
 {
-    char **arena, nome_instancia[50], dimencao[25];
+    char **arena, nome_instancia[50], dimensao[25];
     int saida, orcamento, linha, coluna;
     FILE *entrada;
-    if (argc - 1 == 1)
+    if (argc  == 2)
     {
         entrada = fopen(argv[1], "r");
-        leitura_parametro(nome_instancia, &orcamento, &saida, dimencao, entrada);
-        separa_dimencao(&linha,&coluna,dimencao);
+        leitura_parametro(nome_instancia, &orcamento, &saida, dimensao, entrada);
+        separa_dimencao(&linha,&coluna,dimensao);
         /*Alocação das linhas da matriz*/
         arena = (char **)malloc(linha * sizeof(char *));
         /*Verificação da alocação*/
         if (arena != NULL)
         {
-            aloca_arena(arena, coluna);
-            leitura_arena(arena, entrada);
+            aloca_arena(arena, coluna, linha);
+            leitura_arena(arena, entrada, linha);
             fclose(entrada);
-            mostrar_arena(arena);
+            mostrar_arena(arena, linha);
         }
         else
         {
@@ -102,7 +119,7 @@ int main(int argc, char *argv[])
             exit(1);
         }
         /*Liberaçao do espaço alocado*/
-        libera_arena(arena);
+        libera_arena(arena,linha);
     }
     else
     {
